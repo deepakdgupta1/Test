f4981cf11e526f3005a888965c072651ae995305
Deepak <121713369+deepakdgupta1@users.noreply.github.com>
2025-02-16 14:16:14 +0530

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,1 +1,9 @@
+"""Tests for utils module."""
 
+from my_project.utils import add_numbers
+
+def test_add_numbers():
+    """Test add_numbers function."""
+    assert add_numbers(1, 2) == 3
+    assert add_numbers(-1, 1) == 0
+    assert add_numbers(0, 0) == 0
