5ae053b6eb225d9d74c117add0e9ca92ff6c49f1
Deepak <121713369+deepakdgupta1@users.noreply.github.com>
2025-02-16 14:15:30 +0530

    Update utils.py

--- src/my_project/utils.py
+++ src/my_project/utils.py
@@ -1,1 +1,14 @@
+"""Utility functions for the project."""
+
+def add_numbers(a: int, b: int) -> int:
+    """Add two numbers together.
+    
+    Args:
+        a: First number
+        b: Second number
+        
+    Returns:
+        Sum of a and b
+    """
+    return a + b
 
